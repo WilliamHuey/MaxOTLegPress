3202cef56d4b8a1f943a29b68fe019a40e54cc6e
William <lliw1236@yahoo.com>
2012-01-31 16:13:50 -0800

    Application Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaxOTLegPress::Application.config.secret_token = '6385cddd91cbd0cc0f6115994327d69230e9dc1ee65109dd7f0a6ed02afe4af315500d895110c1f22730b7a3896141dd766d9dd219e1f1022b5da8be9b44f602'
