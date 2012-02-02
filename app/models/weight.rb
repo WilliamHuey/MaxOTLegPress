43fd7fb5881f046265e56a99bee2b1a543e8a87a
William <lliw1236@yahoo.com>
2012-02-01 19:47:25 -0800

    half weight for table

--- app/models/weight.rb
+++ app/models/weight.rb
@@ -27,4 +27,32 @@ class Weight
       :num5 => num5
     }
   end
+  def self.wills_calculate_plates(weight)
+    num45 = weight / 45
+    num35 =
+        (weight - (45 * num45)) / 35
+    num25 =
+      (weight -
+      (45 * num45) -
+      (35 * num35)) / 25
+    num10 =
+      (weight -
+      (45 * num45) -
+      (35 * num35) -
+      (25 * num25)) / 10
+    num5 =
+      (weight -
+      (45 * num45) -
+      (35 * num35) -
+      (25 * num25) -
+      (10 * num10)) / 5
+
+    return {
+      :num45 => num45,
+      :num35 => num35,
+      :num25 => num25,
+      :num10 => num10,
+      :num5 => num5
+    }
+  end
 end
